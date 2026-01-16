aaf8dad253ecaaf0f4e54c2173224a4e410f6fd2
creativebreed <121494356+creativebreed@users.noreply.github.com>
2026-01-16 18:22:00 +0000

    Add dynamic and revalidate exports to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ScribbleApp from "@/components/ScribbleApp";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default function Page() {
   return <ScribbleApp />;
 }
